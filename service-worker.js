cd4ffff009ab40d10c8b20d5c1be1afabcfcaf7b
lazonadeldj <rolimixdj20@gmail.com>
2025-08-29 13:06:56 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'la-super-estacion-593-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/radio_lasuperestacion_PROYECTO_BASE64.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
